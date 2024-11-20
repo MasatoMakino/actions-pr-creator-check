d103cb41a522682bc7d5d333c2857d43414750a3
MasatoMakino <makino.masato.g@gmail.com>
2024-11-20 14:13:51 +0900

    fix: format arguments in pull request creation and merging commands

--- bin/postVersion/pullRequest.mjs
+++ bin/postVersion/pullRequest.mjs
@@ -2,5 +2,13 @@ import { execa } from "execa";
 import { getTagBranchName } from "../getTagVersion.mjs";
 
 const branchName = await getTagBranchName();
-await execa("gh", ["pr create", "--fill", "--base main", "--head", branchName]);
-await execa("gh", ["pr merge", branchName, "--merge", "--auto"]);
+await execa("gh", [
+  "pr",
+  "create",
+  "--fill",
+  "--base",
+  "main",
+  "--head",
+  branchName,
+]);
+await execa("gh", ["pr", "merge", branchName, "--merge", "--auto"]);
