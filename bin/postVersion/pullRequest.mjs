e1527870522e0f0907c6d3f4e2fe9e68dbfa976e
MasatoMakino <makino.masato.g@gmail.com>
2024-11-20 12:58:31 +0900

    feat : add script for automated pull request creation and merging

---
+++ bin/postVersion/pullRequest.mjs
@@ -0,0 +1,6 @@
+import { execa } from "execa";
+import { getTagBranchName } from "../getTagVersion.mjs";
+
+const branchName = await getTagBranchName();
+await execa("gh", ["pr create", "--fill", "--base main", "--head", branchName]);
+await execa("gh", ["pr merge", branchName, "--merge", "--auto"]);
