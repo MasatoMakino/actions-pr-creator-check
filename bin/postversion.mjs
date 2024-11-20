211c68938b14bd8e417aeeb54b0b684db4130a40
MasatoMakino <makino.masato.g@gmail.com>
2024-11-20 22:38:56 +0900

    feat: restore tag checks and pushing in release function, remove from postversion function

--- bin/postversion.mjs
+++ bin/postversion.mjs
@@ -4,16 +4,12 @@ import {
   push,
   pullRequest,
 } from "./postVersion/index.mjs";
-import { checkTagExists, pushTags } from "./release/index.mjs";
 
 export async function postversion() {
   try {
-    await checkTagExists();
-
     await addPackageFiles();
     await checkout();
     await push();
-    await pushTags();
     await pullRequest();
   } catch (e) {
     throw e;
