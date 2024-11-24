6fb8c253fd31f8a55615d2da17807663ef15aab9
MasatoMakino <makino.masato.g@gmail.com>
2024-11-24 17:24:41 +0900

    feat: add git fetch for default branch in preversion function

--- src/preversion.ts
+++ src/preversion.ts
@@ -2,6 +2,7 @@ import { CommandOptions } from "CommandOptions.js";
 import { execa } from "execa";
 
 export async function preversion(options: CommandOptions): Promise<void> {
+  await execa("git", ["fetch", "origin", options.defaultBranch]);
   await execa("git", ["checkout", options.defaultBranch]);
   await execa("git", ["pull", "origin", options.defaultBranch]);
   await execa("npm", ["ci"]);
