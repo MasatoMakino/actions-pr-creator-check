208a55364fa44b4714eb66a97a47cf170b0b8afa
MasatoMakino <makino.masato.g@gmail.com>
2024-12-01 15:56:29 +0900

    getCheckStatus関数のタイムアウトメッセージを改善し、チェック結果の処理を整理

--- src/postVersion/getCheckState.ts
+++ src/postVersion/getCheckState.ts
@@ -26,29 +26,45 @@ export async function getCheckStatus(prURL: string) {
 				// if checks are failed, return
 				if (result.some((check) => check.state === "FAILURE")) {
 					console.log("Some checks are failed");
+					clearInterval(intervalId);
 					resolve("failed");
 				}
 
 				// all checks are successful
 				if (result.every((check) => check.state === "SUCCESS")) {
 					console.log("All required checks are successful");
+					clearInterval(intervalId);
 					resolve("success");
 				}
 
 				if (Date.now() - startTime >= timeout) {
-					console.log("Timeout: Checks did not complete within 180 seconds");
+					console.log(
+						`Timeout: Checks did not complete within ${timeout / 1000} seconds`,
+					);
+					clearInterval(intervalId);
 					resolve("timeout");
 				}
 			} catch (e) {
 				// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 				if ((e as any).stderr.includes("no required checks reported")) {
 					console.log("No required checks reported");
+					clearInterval(intervalId);
 					resolve("failed");
 				}
 				// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 				if ((e as any).stderr.includes("no checks reported")) {
 					return;
 				}
+
+				if (Date.now() - startTime >= timeout) {
+					console.log(
+						`Timeout: Checks did not complete within ${timeout / 1000} seconds`,
+					);
+					clearInterval(intervalId);
+					resolve("timeout");
+				}
+
+				clearInterval(intervalId);
 				reject(e);
 			}
 		};
